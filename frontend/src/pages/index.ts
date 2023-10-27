76e0256bfdd7d0512f5c4c32938efa016a13fa60
theody85 <gyambrahtheodora.8@gmail.com>
2023-10-27 01:12:03 +0000

    fix: errors

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Auth } from "./Auth";
 export { default as NotFound } from "./NotFound";
 export { default as SignUp } from "./SignUp";
+export { default as Home } from "./home/Home";
